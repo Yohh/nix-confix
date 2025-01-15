421e777eeb14d03769d2d87dcced5ee69ed91e35
Yohh <durandyohan@zaclys.net>
2025-01-16 00:39:57 +0100

    nixvim: add transparent

--- nixvimPlugins/nvim_ui.nix
+++ nixvimPlugins/nvim_ui.nix
@@ -75,5 +75,15 @@
     noice = {
       enable = true;
     };
+
+    transparent = {
+      enable = true;
+      settings = {
+        extras_groups = [
+          'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier', 'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function', 'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText', 'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC', 'EndOfBuffer',
+        ];
+      };
+
+    };
   };
 }
